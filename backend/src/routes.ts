9f2a89ac35623b40e4e4fb2a7e0c21222a143831
Carol <carolinaabpessoa@gmail.com>
2025-01-23 23:12:09 -0300

    feat: install dependencies and create error handling middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,3 +2,8 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
+router.get('/teste', (req: Request, res: Response) => {
+    throw new Error('erro no request');
+})
+
+export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors'; // deixar sempre como o segundo import
+import cors from 'cors';
+
+import { router } from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(3333, () => console.log("server rodando")); // pra inicializar o projeto
+app.use(router);
+
+// middleware global p tratar erros - todas as rotas passarão por esse erro
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) { // se for uma instância do tipo erro
+        res.status(400).json({
+            error: err.message
+        })
+    }
+
+    res.status(500).json({
+        message: 'Internal Server Error.'
+    })
+})
+
+app.listen(3333, () => console.log("server rodando...")); // pra inicializar o projeto
