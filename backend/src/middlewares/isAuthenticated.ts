c592a7482fed83934b1100c81e437fffbc311690
Carol <carolinaabpessoa@gmail.com>
2025-01-28 21:51:02 -0300

    feat: created authentication middleware and new route for user details

---
+++ backend/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,33 @@
+/*middleware criado para autenticar os usuarios em rotas que devem ser acessadas 
+somente por usuários cadastrados*/
+
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface Payload {
+    sub: string;
+}
+
+export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+    // primeiro de tudo, receber o token
+    const authToken = req.headers.authorization;
+
+    if(!authToken) {
+        res.status(401).end();
+        return;
+    }
+
+    const [, token] = authToken.split(" ");
+
+    try {
+        // validar token
+        const { sub } = verify(token, process.env.JWT_SECRET as string) as Payload;
+
+        // recuperar o id do token e colocar dentro da variavel "user_id", dentro do request
+        req.user_id = sub;
+
+        return next();
+    } catch (err) {
+        console.error("Erro ao validar token:", err);
+    }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
+// controllers
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+// middlewares
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
 
 const router = Router();
 
 // Rotas - user
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
+router.get('/userinfo', isAuthenticated, new DetailUserController().handle);
 
 export { router };
