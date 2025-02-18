1aa39653f4c93ab457ca6cd01b0aaa1b7ffcba20
Carol <carolinaabpessoa@gmail.com>
2025-02-18 14:04:31 -0300

    feat: add category creation funcionality

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
-// controllers
+// controllers - user
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
+
+//controllers - category
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 // middlewares
 import { isAuthenticated } from './middlewares/isAuthenticated';
@@ -15,5 +18,8 @@ const router = Router();
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/userinfo', isAuthenticated, new DetailUserController().handle);
+
+// Rotas - categories
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
