34381ccce7d109d4582a02d10daea37359c49438
Carol <carolinaabpessoa@gmail.com>
2025-02-19 23:29:08 -0300

    feat: complete product creation logic

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -5,14 +5,22 @@ import { CreateProductService } from "../../services/product/CreateProductServic
 class CreateProductController {
     async handle(req: Request, res: Response) {
         const { name, price, description, category_id } = req.body;
+        
+        const createProductService = new CreateProductService();
 
         if(!req.file) {
             throw new Error("Erro no upload do arquivo");
         }
 
-        const createProductService = new CreateProductService();
+        const { originalname, filename: banner } = req.file;
 
-        const product = await createProductService.execute({name, price, description, banner: '', category_id});
+        const product = await createProductService.execute({
+            name, 
+            price, 
+            description, 
+            banner, 
+            category_id
+        });
 
         res.json(product);
     }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors'; // deixar sempre como o segundo import
 import cors from 'cors';
+import path from 'path';
 
 import { router } from './routes';
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp'))); // criando rota estática pra acesso às imagens
 
 // middleware global p tratar erros - todas as rotas passarão por esse erro
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
