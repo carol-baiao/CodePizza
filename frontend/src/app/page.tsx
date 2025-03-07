1fee106e85940d18d8c3f2745ee763554864f7ab
Carol <carolinaabpessoa@gmail.com>
2025-03-06 21:39:11 -0300

    feat: add login page structure and styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,41 @@
 import styles from './page.module.scss'
+import logoImg from '/public/logo.svg';
+import Image  from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo CodePizza" className={styles.logo}/>
+      
+
+        <section className={styles.login}>
+          <form>
+            <input 
+              type="email"
+              required
+              name='email'
+              placeholder="digite seu email"
+              className={styles.input}
+            />
+
+            <input 
+              type="password"
+              required
+              name='password'
+              placeholder="digite sua senha"
+              className={styles.input}
+            />
+
+            <button type='submit'>Acessar</button>
+          </form>
+
+          <Link href='/signup' className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
