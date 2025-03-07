319c371685c6e71d20114079447ba4b3b7f743eb
Carol <carolinaabpessoa@gmail.com>
2025-03-06 22:40:11 -0300

    feat: add register funcionality - frontend

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,49 +2,76 @@ import styles from '../page.module.scss'
 import logoImg from '/public/logo.svg';
 import Image  from 'next/image';
 import Link from 'next/link';
+import { api } from '@/services/api'
+import { redirect } from 'next/navigation';
 
 export default function Signup() {
-    return(
-        <>
-            <div className={styles.containerCenter}>
-        <Image src={logoImg} alt="Logo CodePizza" className={styles.logo}/>
-      
+  async function handleRegister(formData: FormData) {
+    "use server"
 
-        <section className={styles.login}>
-          <h1>Criando sua conta</h1>
-          <form>
-            <input 
-              type="text"
-              required
-              name='name'
-              placeholder="digite seu nome"
-              className={styles.input}
-            />
-            
-            <input 
-              type="email"
-              required
-              name='email'
-              placeholder="digite seu email"
-              className={styles.input}
-            />
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const password = formData.get('password');
 
-            <input 
-              type="password"
-              required
-              name='password'
-              placeholder="digite sua senha"
-              className={styles.input}
-            />
+    if (name === "" || email === "" || password === "") {
+      console.log("preencha todos os campos");
+      return;
+    }
 
-            <button type='submit'>Cadastrar</button>
-          </form>
+    try {
+      await api.post("/users", {
+        name,
+        email,
+        password,
+      })
+    } catch (err) {
+      console.log(err)
+    }
 
-          <Link href='/' className={styles.text}>
-            Já possui uma conta? Faça o login
-          </Link>
-        </section>
-      </div>
-        </>
-    )
+    redirect('/');
+  }
+
+  return(
+      <>
+          <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo CodePizza" className={styles.logo}/>
+    
+
+      <section className={styles.login}>
+        <h1>Criando sua conta</h1>
+        <form action={handleRegister}>
+          <input 
+            type="text"
+            required
+            name='name'
+            placeholder="digite seu nome"
+            className={styles.input}
+          />
+          
+          <input 
+            type="email"
+            required
+            name='email'
+            placeholder="digite seu email"
+            className={styles.input}
+          />
+
+          <input 
+            type="password"
+            required
+            name='password'
+            placeholder="digite sua senha"
+            className={styles.input}
+          />
+
+          <button type='submit'>Cadastrar</button>
+        </form>
+
+        <Link href='/' className={styles.text}>
+          Já possui uma conta? Faça o login
+        </Link>
+      </section>
+    </div>
+      </>
+  )
 }
