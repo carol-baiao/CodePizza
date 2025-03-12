8f178cc4fc22d887c4ff71f05632d141a7b193ad
Carol <carolinaabpessoa@gmail.com>
2025-03-12 00:44:35 -0300

    feat: create category page

---
+++ frontend/src/app/dashboard/category/page.tsx
@@ -0,0 +1,27 @@
+import styles from './styles.module.scss'
+import { Button } from '@/app/dashboard/components/button'
+
+export default function Category() {
+    async function handleRegisterCategory() {
+        "use server"
+        
+    }
+
+    return(
+        <main className={styles.container}>
+            <h1>Nova categoria</h1>
+
+            <form className={styles.form} action={handleRegisterCategory}>
+                <input 
+                    type="text" 
+                    name="name" 
+                    placeholder="nome da categoria"
+                    required
+                    className={styles.input} 
+                />
+
+                <Button name='Cadastrar'/>
+            </form>
+        </main>
+    )
+}
---
+++ frontend/src/app/dashboard/menu/page.tsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+    return(
+        <main>
+            <h1>cardapio</h1>
+        </main>
+    )
+}
