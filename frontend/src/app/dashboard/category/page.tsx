8ccaea927dce3c56a6b575e7ccaa29114ecf6ae4
Carol <carolinaabpessoa@gmail.com>
2025-03-13 12:01:45 -0300

    feat: add logic to create category

--- frontend/src/app/dashboard/category/page.tsx
+++ frontend/src/app/dashboard/category/page.tsx
@@ -1,10 +1,29 @@
 import styles from './styles.module.scss'
 import { Button } from '@/app/dashboard/components/button'
+import { api } from '@/services/api';
+import { getCookieServer } from '@/lib/cookieServer';
 
 export default function Category() {
-    async function handleRegisterCategory() {
+    async function handleRegisterCategory(formData: FormData) {
         "use server"
-        
+        const name = formData.get('name') as string;
+
+        if(name.trim() === "") return; 
+
+        const data = {
+            name: name,
+        }
+
+        const token = await getCookieServer();
+
+        await api.post('/category', data, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     }
 
     return(
